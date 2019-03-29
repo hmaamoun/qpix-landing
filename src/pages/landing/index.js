f0e480a24983724016f76c6ec729fcfdab418be1
george-maged <george@qpix.io>
2019-03-29 22:36:22 +0200

    Added basic scroll functionality

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Carousel } from 'antd';
 import styled from 'styled-components';
-const primaryColor = '#343b48'
+const primaryColor = '#343b48';
+const yellow = '#ede125';
+const green = '#73c383'
+const dotColors = ['white', primaryColor, yellow, green]
 const StyledCarousel = styled(Carousel)`
     .slick-list{
         height:100vh;
@@ -14,32 +17,61 @@ const StyledCarousel = styled(Carousel)`
         width:16px;
         height:16px;
         border-radius:16px;
+        border: 2px solid ${props => props.color};
+        background-color:transparent;
+        opacity:1;
     }
     .slick-dots li.slick-active button {
         width:16px;
         height:16px;
         border-radius:16px;
+        background-color:${props => props.color}
     }
 `
 const CarouselItem = styled.div`
     height:100vh;
+    background:${props => props.color};
 `
 export default class BlogIndexPage extends React.Component {
-    render() {
-        console.log(Carousel)
-        return (
-            <div>
+    state = {
+        currentColor: dotColors[0]
+    }
+    componentDidMount() {
+        this.slider.goTo(0);
+        window.addEventListener('wheel', this.handleScroll);
 
-                <StyledCarousel ref={slider => (this.slider = slider)} vertical>
-                    <CarouselItem><h3><input
+    }
+    componentWillUnmount() {
+        window.removeEventListener('wheel', this.handleScroll);
+    }
+    handleScroll = (e)=>{
+        let delta;
+        if (e.wheelDelta){
+            delta = e.wheelDelta;
+        }else{
+            delta = -1 * e.deltaY;
+        }
+
+        if (delta < 0){
+            this.slider.next();
+        }else if (delta > 0){
+            this.slider.prev();
+        }
+    }
+    render() {
+        const { currentColor } = this.state
+        return (
+            <div onScroll={(x) => console.log(x)}>
+                <StyledCarousel afterChange={(slide) => this.setState({ currentColor: dotColors[slide] })} color={currentColor} ref={slider => (this.slider = slider)} vertical>
+                    <CarouselItem color={'#313846'}><h3><input
                         onChange={e => this.slider.goTo(e.target.value)}
                         type="range"
                         min={0}
                         max={3}
                     /></h3></CarouselItem>
-                    <CarouselItem><h3>2</h3></CarouselItem>
-                    <CarouselItem><h3>3</h3></CarouselItem>
-                    <CarouselItem><h3>4</h3></CarouselItem>
+                    <CarouselItem color={'white'}><h3>2</h3></CarouselItem>
+                    <CarouselItem color={'#313846'}><h3>3</h3></CarouselItem>
+                    <CarouselItem color={'white'}><h3>4</h3></CarouselItem>
                 </StyledCarousel>
             </div>
         )
